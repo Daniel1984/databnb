a0a5eeecb31bd4e7e5328822384dd23e233fdd51
daniel <aciddaniel@gmail.com>
2018-07-21 01:07:58 +0200

    informative scraper

--- jobs/updateExistingListingsData.js
+++ jobs/updateExistingListingsData.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.DB_URI)
     const listing = listings.shift();
     const availabilityUrl = getAvailabilityUrl({ listingId: listing.id });
 
-    console.log(`Updating listing: ${listing._id}`);
+    console.log(`Updating listing: ${listing._id}, ${listings.length} left`);
 
     try {
       const availabilities = await getListingAvailabilities(availabilityUrl);
