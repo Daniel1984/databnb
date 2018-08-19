4a8d734a848af0cca394790c2604a5efd0abaf77
daniel <aciddaniel@gmail.com>
2018-08-19 22:18:28 +0300

    control flow fix

--- routes/updateStreet/updateStreet.js
+++ routes/updateStreet/updateStreet.js
@@ -4,8 +4,8 @@ const Neighborhood = require('../../models/neighborhood');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-  await Neighborhood.findByIdAndUpdate(req.body._id, { listingsScrapedAt: Date.now() });
   try {
+    await Neighborhood.findByIdAndUpdate(req.body._id, { listingsScrapedAt: Date.now() });
     res.status(200).json({ status: 'OK' });
   } catch (error) {
     res.status(500).json({ err: 'server error', error });
